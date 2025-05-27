01e2a7aa0fda164634f0924dc55ab0735e72eea8
Suhani <suhanigohel1022k15@gmail.com>
2025-05-27 17:15:38 +0000

    basics

--- one.js
+++ one.js
@@ -3,7 +3,7 @@ const promiseOne = new Promise(function(resolve ,  reject) {
     // db , crptography , network
     setTimeout(function(){
         console.log("Async task is completed");
-        
+        resolve()
     } , 1000)
 })
 
@@ -11,3 +11,31 @@ promiseOne.then(function(){
     console.log("Promise consumed");
     
 })
+
+// second 
+
+new Promise(function(resolve , reject) {
+    setTimeout(function() {
+        console.log("Async 2");
+        resolve()
+    } , 2000)
+}).then(function() {
+    console.log("Promise 2 resolved");
+    
+})
+
+//third
+
+const promiseThree = new Promise(function(res , rej) {
+    setTimeout(function() {
+       res({username: "Suhani" , surname: "Gohel"})
+    } , 3000)
+})
+
+promiseThree.then(function(user) {
+    console.log(user);
+    
+})
+
+// fourth
+
